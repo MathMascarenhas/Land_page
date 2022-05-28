fa7e9c60d29c775afcf7c693f37a174d4984fe5a
Matheus Mascarenhas <mathmasca@gmail.com>
2022-05-27 22:22:50 -0300

    commit on main

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.urlencoded());
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+let message = "";
+
+app.get("/", (req, res) => {
+    const devList = ["Backend", "Frontend", "Fullstack"];
+    const analyticsList = ["Engenharia de dados", "Ciência de dados"];
+
+    setTimeout(() => {
+      message = "";
+    }, 1000);
+
+    res.render("index", { titulo: "Blue", devList: devList, analyticsList: analyticsList, message});
+});
+
+app.post("/subscription", (req, res) => {
+   const {nome, email} = req.body; 
+   message = `Parabéns ${nome}, sua inscrição foi realizada com sucesso! Um e-mail foi enviado para: ${email}`;
+   res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodado em http://localhost:3000/")}
+  );
